0a7c503d532f61abcf8821669d825e5315b634fd
FooQoo <horseshoes.buzz.17@gmail.com>
2019-08-04 22:19:30 +0900

    fix bug

--- env.sh
+++ env.sh
@@ -1,14 +1,14 @@
-read -p "SECRET_KEY:" SECRET_KEY
-read -p "NAME:" NAME
-read -p "HOST:" HOST
-read -p "USER:" USER
-read -p "PORT:" PORT
-read -p "PASSWORD:" PASSWORD
+read -p "DJANGO_SECRET_KEY:" DJANGO_SECRET_KEY
+read -p "DB_NAME:" DB_NAME
+read -p "DB_HOST:" DB_HOST
+read -p "DB_USER:" DB_USER
+read -p "DB_PORT:" DB_PORT
+read -p "DB_PASSWORD:" DB_PASSWORD
 
-heroku config:add SECRET_KEY="${SECRET_KEY}"
-heroku config:add NAME="${NAME}"
-heroku config:add HOST="${HOST}"
-heroku config:add USER="${USER}"
-heroku config:add PORT="${PORT}"
-heroku config:add PASSWORD="${PASSWORD}"
+heroku config:add DJANGO_SECRET_KEY="${DJANGO_SECRET_KEY}"
+heroku config:add DB_NAME="${DB_NAME}"
+heroku config:add DB_HOST="${DB_HOST}"
+heroku config:add DB_USER="${DB_USER}"
+heroku config:add DB_PORT="${DB_PORT}"
+heroku config:add DB_PASSWORD="${DB_PASSWORD}"
 heroku config:set DISABLE_COLLECTSTATIC=1
